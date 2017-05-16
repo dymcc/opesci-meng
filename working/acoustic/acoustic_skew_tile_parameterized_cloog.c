704eb10580879d46287ec3708e8822be6aec75c3
Dylan McCormick <dm2413@ic.ac.uk>
2017-05-16 17:37:21 +0100

    Paramaterized stencil for tile dimension changing

--- working/acoustic/acoustic_skew_tile_parameterized_cloog.c
+++ working/acoustic/acoustic_skew_tile_parameterized_cloog.c
@@ -1,4 +1,4 @@
-/* Generated from acoustic_skew_tile_parameterized.cloog by CLooG 0.18.4-b2b4f77 gmp bits in 0.02s. */
+/* Generated from acoustic_skew_tile_parameterized.cloog by CLooG 0.18.4-b2b4f77 gmp bits in 0.03s. */
 /* DON'T FORGET TO USE -lm OPTION TO COMPILE. */
 
 /* Useful headers. */
@@ -35,14 +35,13 @@ int main() {
   int M=PARVAL1, N=PARVAL2, O=PARVAL3, P=PARVAL4;
   int total=0;
 
-  int t, xx, yy, zz, x, y, z;
-  for (t=0;t<=82;t++) {
-    for (xx=0;xx<=5;xx++) {
-      for (yy=0;yy<=5;yy++) {
-        for (zz=0;zz<=5;zz++) {
-          for (x=max(4*t+4,4*t+8*xx);x<=4*t+8*xx+7;x++) {
-            for (y=max(4*t+4,4*t+8*yy);y<=4*t+8*yy+7;y++) {
-              for (z=max(4*t+4,4*t+8*zz);z<=4*t+8*zz+7;z++) {
+  for (t=1;t<=81;t++) {
+    for (xx=0;xx<=7;xx++) {
+      for (yy=0;yy<=7;yy++) {
+        for (zz=0;zz<=7;zz++) {
+          for (x=max(4*t+4,4*t+27*xx);x<=min(4*t+211,4*t+27*xx+26);x++) {
+            for (y=max(4*t+4,4*t+27*yy);y<=min(4*t+211,4*t+27*yy+26);y++) {
+              for (z=max(4*t+4,4*t+27*zz);z<=min(4*t+211,4*t+27*zz+26);z++) {
                 S1(t,xx,yy,zz,(-4*t+x),(-4*t+y),(-4*t+z));
               }
             }
